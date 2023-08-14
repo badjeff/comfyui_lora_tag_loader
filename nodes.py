1712e5dde0602682bb5eec7721aaee0815c95587
Jeff Leung <badjeff@gmail.com>
2023-08-14 22:07:22 +0800

    init commit

---
+++ nodes.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+import folder_paths
+import re
+
+# Import ComfyUI files
+import comfy.sd
+import comfy.utils
+
+class LoraTagLoader:
+    def __init__(self):
+        self.loaded_lora = None
+        self.tag_pattern = "\<[0-9a-zA-Z\:\_\-\.\s\/]+\>"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "model": ("MODEL",),
+                              "clip": ("CLIP", ),
+                              "text": ("STRING", {"multiline": True}),
+                              }}
+    RETURN_TYPES = ("MODEL", "CLIP", "STRING")
+    RETURN_NAMES = ("MODEL", "CLIP", "STRING")
+    FUNCTION = "load_lora"
+
+    CATEGORY = "loaders"
+
+    def load_lora(self, model, clip, text):
+        # print(f"\nLoraTagLoader input text: { text }")
+
+        founds = re.findall(self.tag_pattern, text)
+        # print(f"\nfoound lora tags: { founds }")
+
+        if len(founds) < 1:
+            return (model, clip, text)
+
+        model_lora = model
+        clip_lora = clip
+        
+        lora_files = folder_paths.get_filename_list("loras")
+        for f in founds:
+            tag = f[1:-1]
+            (type, name, weight) = tag.split(":")
+            if type != 'lora':
+                continue
+            lora_name = None
+            for lora_file in lora_files:
+                if Path(lora_file).name.startswith(name):
+                    lora_name = lora_file
+                    break
+            if lora_name == None:
+                print(f"bypassed lora tag: { (type, name, weight) } >> { lora_name }")
+                continue
+            # print(f"detected lora tag: { (type, name, weight) } >> { lora_name }")
+
+            lora_path = folder_paths.get_full_path("loras", lora_name)
+            lora = None
+            if self.loaded_lora is not None:
+                if self.loaded_lora[0] == lora_path:
+                    lora = self.loaded_lora[1]
+                else:
+                    temp = self.loaded_lora
+                    self.loaded_lora = None
+                    del temp
+
+            if lora is None:
+                lora = comfy.utils.load_torch_file(lora_path, safe_load=True)
+                self.loaded_lora = (lora_path, lora)
+
+            strength_model = float(weight)
+            strength_clip = float(weight)
+            model_lora, clip_lora = comfy.sd.load_lora_for_models(model_lora, clip_lora, lora, strength_model, strength_clip)
+
+        plain_prompt = re.sub(self.tag_pattern, "", text)
+        return (model_lora, clip_lora, plain_prompt)
+
+NODE_CLASS_MAPPINGS = {
+    "LoraTagLoader": LoraTagLoader,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    # Loaders
+    "LoraTagLoader": "Load LoRA Tag",
+}
